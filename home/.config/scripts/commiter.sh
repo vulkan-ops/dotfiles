d04af9e1094ae79036ee2fb98365e406b6d3d447
vulkan-ops <vulkanops@gmail.com>
2021-03-26 02:10:40 -0300

    adapting commiter.sh for my use

--- home/.config/scripts/commiter.sh
+++ home/.config/scripts/commiter.sh
@@ -1,45 +1,32 @@
 #!/usr/bin/env bash
 
-# Autocommit
-# manual-custom-rom
+github=/home/vitor/GitHub
 
-github=$HOME/GitHub
-kraken=$HOME/AOSPK
+repo0="dotfiles"
+repo1="arch"
+repo2="ocean-dt"
+repo3="common-tree"
+repo4="vendor-ocean"
+repo5="vendor-common"
+repo6="kernel"
+repos="$repo0\n$repo1\n$repo2\n$repo3\n$repo4\n$repo5\n$repo6"
 
-repo1="dotfiles"
-repo2="mamutal91.github.io"
-repo3="myarch"
-repo4="infra"
-repo5="language-swaywm"
-repo6="shellscript-atom-snippets"
-repo7="AOSPK/manifest"
-repo8="Poco F2/device_xiaomi_lmi"
-repo9="Poco F2/device_xiaomi_sm8250-common"
-repo10="BuildersBR/buildersbr"
-
-repos="$repo1\n$repo2\n$repo3\n$repo4\n$repo5\n$repo6\n$repo7\n$repo8\n$repo9\n$repo10"
-
-chosen="$(echo -e "$repos" | wofi --lines 10 --sort-order=DEFAULT --dmenu -p "  Commiter")"
+chosen="$(echo -e "$repos" | wofi --lines 11 --dmenu -p "  Commiter")"
 case $chosen in
-    $repo1)
-        alacritty -t newcommit --working-directory $HOME/.dotfiles;;
-    $repo2)
-        alacritty -t newcommit --working-directory $github/mamutal91.github.io;;
-    $repo3)
-        alacritty -t newcommit --working-directory $github/myarch;;
-    $repo4)
-        alacritty -t newcommit --working-directory $github/infra;;
-    $repo5)
-        alacritty -t newcommit --working-directory $github/language-swaywm;;
-    $repo6)
-        alacritty -t newcommit --working-directory $github/shellscript-atom-snippets;;
-    $repo7)
-        alacritty -t newcommit --working-directory $kraken/manifest;;
-    $repo8)
-        alacritty -t newcommit --working-directory $github/device_xiaomi_lmi;;
-    $repo9)
-        alacritty -t newcommit --working-directory $github/device_xiaomi_sm8250-common;;
-    $repo10)
-        alacritty -t newcommit --working-directory $github/buildersbr;;
+$repo0)
+alacritty -t newcommit --working-directory $HOME/.dotfiles;;
+$repo1)
+alacritty -t newcommit --working-directory $github/arch;;
+$repo2)
+alacritty -t newcommit --working-directory $github/ocean-dt;;
+$repo3)
+alacritty -t newcommit --working-directory $github/common-tree;;
+$repo4)
+alacritty -t newcommit --working-directory $github/vendor-ocean;;
+$repo5)
+alacritty -t newcommit --working-directory $github/vendor-common;;
+$repo6)
+alacritty -t newcommit --working-directory $github/kernel;;
+
 esac
 exit 0;
